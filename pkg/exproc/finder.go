d9da02b001858becea2ed97c899c8db117f80003
Arthur Petukhovsky <petuhovskiy@yandex.ru>
2020-11-30 17:41:25 +0300

    Improve finder

--- pkg/exproc/finder.go
+++ pkg/exproc/finder.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"strings"
 
-	chatexsdk "github.com/chatex-com/sdk-go"
 	"github.com/davecgh/go-spew/spew"
 	"github.com/shopspring/decimal"
 	log "github.com/sirupsen/logrus"
+
+	chatexsdk "github.com/chatex-com/sdk-go"
 
 	"github.com/rwlist/autotrade-bot/pkg/money"
 	"github.com/rwlist/autotrade-bot/pkg/trade/chatex"
@@ -70,8 +71,8 @@ func (f *Finder) OnSnapshot(snap chatex.OrdersSnapshot) {
 
 		rate := money.One.DivRound(reverseRate, money.Precision)
 
-		coin1 := tmp[0]
-		coin2 := tmp[1]
+		coin1 := tmp[1]
+		coin2 := tmp[0]
 
 		g[coin1][coin2] = rate
 	}
@@ -106,18 +107,72 @@ func (f *Finder) OnSnapshot(snap chatex.OrdersSnapshot) {
 
 			places := int32(5)
 
+			order1 := snap.Fetched[next+"/"+start].Orders[0]
+			order2 := snap.Fetched[start+"/"+next].Orders[0]
+
 			info := fmt.Sprintf(
-				"Found: %s %s → %s %s → %s %s.\tOrders %v and %v",
+				"Found: %s %s → %s %s → %s %s.",
 				money.One.Round(places),
 				start,
 				go1.Round(places),
 				next,
-				have,
+				have.Round(places),
 				start,
-				snap.Fetched[next+"/"+start].Orders[0].ID,
-				snap.Fetched[start+"/"+next].Orders[0].ID,
 			)
 
+			// next -> start
+			midAmount1 := order1.Amount
+
+			// next <- last_start
+			midAmount2 := order2.Amount.Mul(order2.Rate)
+
+			// take min
+			midAmount := decimal.Min(midAmount1, midAmount2)
+
+			// start
+			startAmount := midAmount.Mul(order1.Rate)
+
+			// apply order1
+			nextAmount := startAmount.DivRound(order1.Rate, money.Precision)
+
+			buy1 := fmt.Sprintf(
+				"Buy %s %s for %s %s, orderID = %v",
+				nextAmount.Round(places),
+				next,
+				startAmount.Round(places),
+				start,
+				order1.ID,
+			)
+
+			// apply order2
+			lastAmount := nextAmount.DivRound(order2.Rate, money.Precision)
+
+			buy2 := fmt.Sprintf(
+				"Buy %s %s for %s %s, orderID = %v",
+				lastAmount.Round(places),
+				start,
+				nextAmount.Round(places),
+				next,
+				order2.ID,
+			)
+
+			log.WithFields(log.Fields{
+				"order1":      order1,
+				"order2":      order2,
+				"start":       start,
+				"next":        next,
+				"info":        info,
+				"buy1":        buy1,
+				"buy2":        buy2,
+				"midAmount1":  midAmount1,
+				"midAmount2":  midAmount2,
+				"midAmount":   midAmount,
+				"startAmount": startAmount,
+				"nextAmount":  nextAmount,
+				"lastAmount":  lastAmount,
+			}).Info("found positive loop")
+
+			info = info + "\n* " + buy1 + "\n* " + buy2
 			results = append(results, info)
 		}
 	}
@@ -127,5 +182,5 @@ func (f *Finder) OnSnapshot(snap chatex.OrdersSnapshot) {
 		return
 	}
 
-	f.sender.Send(strings.Join(results, "\n"))
+	f.sender.Send(strings.Join(results, "\n\n"))
 }
